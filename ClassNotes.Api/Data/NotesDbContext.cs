cca3211b5093d39ae71dcb8ec9de3995f1d71eb4
binmarkoo <101064382+binmarkoo@users.noreply.github.com>
2026-01-08 12:05:50 +0100

    finished classnotes

---
+++ ClassNotes.Api/Data/NotesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ClassNotes.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassNotes.Api.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options) { }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ ClassNotes.Api/Program.cs
@@ -0,0 +1,80 @@
+﻿using ClassNotes.Api.Data;
+using ClassNotes.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<NotesDbContext>(options => options.UseSqlite("Data Source= ./Data/notes.db"));
+
+// CORS konfigurieren
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "https://localhost:7185")
+        //policy.WithOrigins("https://diwiatschool.github.io")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+//builder.Services.AddCors();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
+}
+
+app.UseCors("AllowReactApp");
+
+//app.UseHttpsRedirection();
+
+app.MapGet("/notes", async (NotesDbContext db) =>
+{
+    return await db.Notes.OrderBy(n => n.Id).ToListAsync();
+});
+
+app.MapGet("/notes1", GetAllNotes);
+
+List<Note> GetAllNotes(NotesDbContext db)
+{
+    return db.Notes.ToList();
+}
+
+// Eine Notiz nach ID
+app.MapGet("/notes/{id}", async (int id, NotesDbContext db) =>
+{
+    return await db.Notes.FindAsync(id) is Note note
+        ? Results.Ok(note)
+        : Results.NotFound();
+});
+
+// Neue Notiz
+app.MapPost("/notes", async (Note note, NotesDbContext db) =>
+{
+    db.Notes.Add(note);
+    await db.SaveChangesAsync();
+    return Results.Created($"/notes/{note.Id}", note);
+});
+
+// Notiz loeschen
+app.MapDelete("/notes/{id}", async (int id, NotesDbContext db) =>
+{
+    var note = await db.Notes.FindAsync(id);
+    if (note is null) return Results.NotFound();
+    db.Notes.Remove(note);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.Run();
