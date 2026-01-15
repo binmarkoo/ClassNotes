9d9c875891e0d24026b6adcf27e929d38becb1df
binmarkoo <101064382+binmarkoo@users.noreply.github.com>
2026-01-15 10:16:52 +0100

    refactor: streamline Swagger service registration and remove unused OpenAPI mapping

--- ClassNotes.Api/Program.cs
+++ ClassNotes.Api/Program.cs
@@ -21,15 +21,13 @@ builder.Services.AddCors(options =>
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-// Add Swagger 
+// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.MapOpenApi();
+// Swagger aktivieren
 app.UseSwagger();
 app.UseSwaggerUI();
 
